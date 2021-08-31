e13c963bc45cfd78bb607d02195d6a75a3a8a5ae
Parvathy Krishnan K <42402451+parvathykrishnank@users.noreply.github.com>
2021-08-31 09:38:39 +0200

    Create haversine_vectorize.py

---
+++ Scripts/Accessibility Analysis/haversine_vectorize.py
@@ -0,0 +1,8 @@
+def haversine_vectorize(lon1, lat1, lon2, lat2):
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+    newlon = lon2 - lon1
+    newlat = lat2 - lat1
+    haver_formula = np.sin(newlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(newlon/2.0)**2
+    dist = 2 * np.arcsin(np.sqrt(haver_formula ))
+    km = 6367 * dist #6367 for distance in KM for miles use 3958
+    return round(km,2)
